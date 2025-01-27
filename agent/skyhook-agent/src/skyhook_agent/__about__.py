909c74f3298279a81d0a39b3467c854c4e5846cd
Alex Yuskauskas <ayuskauskas@nvidia.com>
2025-01-27 12:35:16 -0800

    feat(agent): add container build ci on commit with attestation

--- agent/skyhook-agent/src/skyhook_agent/__about__.py
+++ agent/skyhook-agent/src/skyhook_agent/__about__.py
@@ -1,4 +1,4 @@
 #  SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #  SPDX-License-Identifier: Apache-2.0
 #
-__version__ = "0.0.6"
+__version__ = "0.0.0"
